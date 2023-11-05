01f415ef1a99b20c0c4d37423761f77afc5bf16f
saldray <othernother@yandex.com>
2023-11-05 15:03:46 +0300

    Localde gitignore ve klasor eklendi

---
+++ benBilirim/largest.py
@@ -0,0 +1,44 @@
+# def getLargest():
+#     numElements = int(input("Enter the number of element you want to add to list: "))
+#     myList = []
+#     for itervar in range(numElements):
+#         element = int(input(f"Enter element {itervar+1}: "))
+#         myList.append(element)
+#     print(myList)
+
+#     largest = None
+#     print('Before:', largest)
+#     for iter in myList:
+#         if largest is None or iter > largest:
+#             largest = iter
+#         print('Loop', iter, largest)
+
+#     print("Largest: ", largest)
+
+#     getLargest()
+# girilecek sayılar [3, 41, 12, 9, 74, 15]
+
+
+
+
+
+
+
+
+# def getLargest():
+#     myList = []
+#     numElements = int(input("Enter the number of list element you want to add to list: "))
+#     for itervar in range(numElements):
+#         element = int(input(f'Enter element {itervar+1}: '))
+#         myList.append(element)
+#     print(element)
+
+#     largest = None
+#     print('Before:', largest)
+#     for iter in myList:
+#         if largest is None or iter > largest:
+#             largest = iter
+#         print('Loop:', iter, largest)
+#     print("Largest:", largest)
+
+# getLargest()
